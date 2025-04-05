70e6ab1299e5d50e38dcc2595359e6482895b859
nickelwrangler446 <thebradycsmith@gmail.com>
2025-04-04 19:43:30 -0500

    Creation

---
+++ src/client/java/com/sb/doom/DoomClient.java
@@ -0,0 +1,10 @@
+package com.sb.doom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DoomClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
